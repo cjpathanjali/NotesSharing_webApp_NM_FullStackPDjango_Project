f829b9dee7861b2c3994d2f79484053e25a57638
Pathanjali Cinnakonda Janardhanan <115952808+cjpathanjali@users.noreply.github.com>
2024-04-10 00:43:03 +0530

    Delete no_access directory

--- no_access/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class NotfoundConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'no_access'
--- no_access/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-
-def no_access(request):
-    template = loader.get_template('no_access.html')
-    return HttpResponse(template.render())
